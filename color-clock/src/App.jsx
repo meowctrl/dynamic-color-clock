13e16e6c52c8cb947ef14b2d59b4a01bb74dc6e4
carol <carolineallen1012@gmail.com>
2026-02-21 16:59:53 -0500

    final commit

--- color-clock/src/App.jsx
+++ color-clock/src/App.jsx
@@ -1,11 +1,36 @@
-import './App.css';
-import { format } from 'date-fns';
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { format } from "date-fns";
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-   <div className='clock-container'> 
-    <p className='clock-text'>{format(new Date(),  'PPpp')}</p>
-   </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          {format(new Date(), 'MM/dd/yyyy h:mm a')
+          }
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
